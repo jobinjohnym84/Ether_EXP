e1db7eec5c99a4d190c703a93222fdb913fb8ccd
Jobin Johny <37933678+jobinjohnym84@users.noreply.github.com>
2018-10-17 00:40:56 -0400

    Matrix View
    Matrix View

--- tscontract.sol
+++ tscontract.sol
@@ -2,40 +2,63 @@ pragma solidity ^0.4.0;
 
 contract TsContract {
 
-    struct TsData {
+    struct Ts111GDP {
 
         string Indi_Code;
         string x2000A;
         string x2001A;
         string x2002A;
-        string x2003A;
-        string x2004A;
+     
+    }
+    
+     struct Ts112GDP {
+
+        string Indi_Code;
+        string x2000A;
+        string x2001A;
+        string x2002A;
+     
+    }
+    
+      struct Ts134GDP {
+
+        string Indi_Code;
+        string x2000A;
+        string x2001A;
+        string x2002A;
      
     }
 
-    TsData[] public TsContracts;
+    Ts111GDP[] public TsContractsV1;
+    Ts112GDP[] public TsContractsV2;
+    Ts134GDP[] public TsContractsV3;
 
  
 
-    function addTs(string _Indi_Code, string _x2000A, string _x2001A, string _x2002A, string _x2003A, string _x2004A ) public returns(uint) {
+    function addTs(string Ts111GDP_Indi_Code, string Ts111GDP_x2000A, string Ts111GDP_x2001A, string Ts111GDP_x2002A, string Ts112GDP_Indi_Code, string Ts112GDP_x2000A , string Ts112GDP_x2001A, string Ts112GDP_x2002A , string Ts134GDP_Indi_Code, string Ts134GDP_x2000A , string Ts134GDP_x2001A, string Ts134GDP_x2002A ) public returns(uint) {
 
-        TsContracts.length++;
+        TsContractsV1.length++;
 
-        TsContracts[TsContracts.length-1].Indi_Code = _Indi_Code;
-
-        TsContracts[TsContracts.length-1].x2000A = _x2000A;
-
-        TsContracts[TsContracts.length-1].x2001A = _x2001A;
+        TsContractsV1[TsContractsV1.length-1].Indi_Code = Ts111GDP_Indi_Code;
+        TsContractsV1[TsContractsV1.length-1].x2000A = Ts111GDP_x2000A;
+        TsContractsV1[TsContractsV1.length-1].x2001A = Ts111GDP_x2001A;
+        TsContractsV1[TsContractsV1.length-1].x2002A = Ts111GDP_x2002A;
         
-        TsContracts[TsContracts.length-1].x2002A = _x2002A;
+        TsContractsV2.length++;
         
-        TsContracts[TsContracts.length-1].x2003A = _x2003A;
+        TsContractsV2[TsContractsV2.length-1].Indi_Code = Ts112GDP_Indi_Code;
+        TsContractsV2[TsContractsV2.length-1].x2000A = Ts112GDP_x2000A;
+        TsContractsV2[TsContractsV2.length-1].x2001A = Ts112GDP_x2001A;
+        TsContractsV2[TsContractsV2.length-1].x2002A = Ts112GDP_x2002A;
         
-        TsContracts[TsContracts.length-1].x2004A = _x2004A;
-        
-     
+         TsContractsV3.length++;
 
-        return TsContracts.length;
+        TsContractsV3[TsContractsV3.length-1].Indi_Code = Ts134GDP_Indi_Code;
+        TsContractsV3[TsContractsV3.length-1].x2000A = Ts134GDP_x2000A;
+        TsContractsV3[TsContractsV3.length-1].x2001A = Ts134GDP_x2001A;
+        TsContractsV3[TsContractsV3.length-1].x2002A = Ts134GDP_x2002A;
+
+        return TsContractsV3.length;
 
     }
 
@@ -43,7 +66,7 @@ contract TsContract {
 
     function getTsCount() public constant returns(uint) {
 
-        return TsContracts.length;
+        return TsContractsV1.length;
 
     }
 
@@ -51,7 +74,14 @@ contract TsContract {
 
     function getTs(uint index) public constant returns(string, string, string , string , string , string ) {
 
-        return (TsContracts[index].Indi_Code, TsContracts[index].x2000A, TsContracts[index].x2001A , TsContracts[index].x2002A , TsContracts[index].x2003A , TsContracts[index].x2004A   );
+        return (TsContractsV1[index].x2000A, TsContractsV1[index].x2001A , TsContractsV1[index].x2002A , TsContractsV2[index].x2000A , TsContractsV2[index].x2001A, TsContractsV2[index].x2002A  );
+        
+
+    }
+    
+     function getTs2(uint index) public constant returns(string, string, string) {
+
+        return (TsContractsV3[index].x2000A, TsContractsV3[index].x2001A , TsContractsV3[index].x2002A );
         
 
     }
